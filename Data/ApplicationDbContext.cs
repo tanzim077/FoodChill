0d0daeff18f2911f2ec4cfe7583dccf090cb2d2f
tanzim.ahmed1994 <tanzim.ahmed1994@outlook.com>
2020-11-12 12:31:27 +0600

    Identity and Partial Class Add

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FoodChill.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,6 @@ namespace FoodChill.Data
             : base(options)
         {
         }
+        public DbSet<Category> Category { get; set; }
     }
 }
