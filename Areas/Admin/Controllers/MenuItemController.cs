9c64d3db33809ce19ff6d5944dbabb6757799f97
tanzim.ahmed1994 <tanzim.ahmed1994@outlook.com>
2020-11-21 00:17:25 +0600

    MenuItem Added

---
+++ Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,46 @@
+﻿using FoodChill.Data;
+using FoodChill.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodChill.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment; //saving Images directly databse
+
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+
+        public MenuItemController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
+        {
+            _db = db;
+            _webHostEnvironment = webHostEnvironment;
+
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItem = new Models.MenuItem()
+            };
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var menuItems = await _db.MenuItem.Include(s => s.Category).Include(s => s.SubCategory).ToListAsync();
+            return View(menuItems);
+        }
+
+        //GEt -- Create
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
+        }
+    }
+}
